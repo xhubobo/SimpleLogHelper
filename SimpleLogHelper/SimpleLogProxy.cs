95b8cb1487a7da6afee625fbd59426a72f9cfe0f
张博 <zhangbo@digibird.com.cn>
2019-06-24 11:09:16 +0800

    + 添加项目文件

---
+++ SimpleLogHelper/SimpleLogProxy.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace SimpleLogHelper
+{
+    public class SimpleLogProxy
+    {
+        private readonly ThreadSafeLog _threadSafeLog;
+        private bool _working = true;
+
+        public void SetLogPath(string logPath)
+        {
+            _threadSafeLog.LogPathHelper.LogPath = logPath;
+        }
+
+        public void SetLogType(LogType logType)
+        {
+            _threadSafeLog.LogPathHelper.LogType = logType;
+        }
+
+        public void Stop()
+        {
+            (_threadSafeLog as IDisposable)?.Dispose();
+            _working = false;
+        }
+
+        /// <summary>
+        /// 写入新日志，根据指定的日志对象Msg
+        /// </summary>
+        /// <param name="msg">日志内容对象</param>
+        public void AddLog(Msg msg)
+        {
+            if (_working)
+            {
+                _threadSafeLog.AddMessage(msg);
+            }
+        }
+
+
+        public void AddLog(string text)
+        {
+            AddLog(new Msg(text));
+        }
+
+        public void AddLog(string text, MsgType type)
+        {
+            AddLog(new Msg(text, type));
+        }
+
+        public void AddLog(Exception e, MsgType type)
+        {
+            AddLog(new Msg(e.Message, type));
+        }
+
+        public void AddLog(DateTime dt, string text, MsgType type)
+        {
+            AddLog(new Msg(dt, text, type));
+        }
+
+        #region 单例模式
+
+        private SimpleLogProxy()
+        {
+            _threadSafeLog = new ThreadSafeLog();
+        }
+
+        private static volatile SimpleLogProxy _instance;
+        private static readonly object LockHelper = new object();
+
+        public static SimpleLogProxy Instance()
+        {
+            if (null != _instance)
+            {
+                return _instance;
+            }
+
+            lock (LockHelper)
+            {
+                if (null == _instance)
+                {
+                    _instance = new SimpleLogProxy();
+                }
+            }
+
+            return _instance;
+        }
+
+        #endregion
+    }
+}
