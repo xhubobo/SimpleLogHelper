4510dad79db1400ce5f1cda089fa955ececc872c
张博 <zhangbo@digibird.com.cn>
2019-07-02 15:28:33 +0800

    + 添加测试项目

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            LogHelper.AddLog("test log");
+            Console.ReadKey();
+        }
+    }
+}
