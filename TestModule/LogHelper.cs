affb16043ac135113e73ad2e27c55cf4f7830a1d
张博 <zhangbo@digibird.com.cn>
2019-07-11 01:51:56 +0800

    + [TestModule] 添加测试库模块TestModule

---
+++ TestModule/LogHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using SimpleLogHelper;
+
+namespace TestModule
+{
+    internal class LogHelper
+    {
+        public static void AddLog(Exception e, MsgType type = MsgType.Error)
+        {
+            AddLog(e.Message, type);
+        }
+
+        public static void AddLog(string msg, MsgType type = MsgType.Information)
+        {
+            var log = new LogHelperInstance();
+            log.AddLog(msg, type);
+        }
+
+        private class LogHelperInstance : SimpleLogHelperTemplate
+        {
+            protected override string AssemblyName { get; } = Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+}
---
+++ TestModule/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestModule
+{
+    public class TestClass
+    {
+        //添加常规日志
+        public static void AddNormalLog()
+        {
+            LogHelper.AddLog("Add normal log in TestClass");
+        }
+
+        //添加异常日志
+        public static void AddExceptionLog()
+        {
+            LogHelper.AddLog(new Exception("Add exception log in TestClass"));
+        }
+    }
+}
